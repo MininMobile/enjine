d9886b6f43407160776b7aafeb3c8ddf4ea82c37
MininMobile <miningamer@gmail.com>
2018-04-15 00:52:14 +0100

    Create Children

--- src/GameManager/gameObject.js
+++ src/GameManager/gameObject.js
@@ -3,9 +3,9 @@
  */
 class GameObject {
 	/**
-	 * 
-	 * @param {string} type Type of Object
+	 * GameObject Class Constructor
 	 * @param {string} id Unique Indentifier for Object
+	 * @param {string} type Type of Object
 	 */
 	constructor(id, type = "box") {
 		switch (type) {
@@ -22,6 +22,18 @@ class GameObject {
 		}
 
 		this.id = id;
+		this.children = [ ];
+	}
+
+	/**
+	 * Create New Child of this GameObject
+	 * @param {string} id 
+	 * @param {string} type 
+	 */
+	AddChild(id, type = "box") {
+		let child = new GameObject(id, type);
+
+		this.children.push(child);
 	}
 }
 
--- src/GameManager/main.js
+++ src/GameManager/main.js
@@ -1,15 +1,17 @@
+const GameObject = require("./gameObject");
+
 /**
  * GameManager Class
  */
-class GameManager {
+class GameManager extends GameObject {
 	/**
 	 * GameManager Class Constructor
 	 * @param {bool} experimental Enables Experimental Functions/Types
 	 */
 	constructor(experimental = false) {
+		super("scene", "canvas");
+
 		this.proto = experimental;
-		
-		this.children = { };
 	}
 }
 
