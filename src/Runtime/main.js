030e85d63bdaae8d235ebaf22a1b2cce4eb06502
MininMobile <miningamer@gmail.com>
2018-04-14 17:14:53 +0100

    Create Runtime Class

--- src/Runtime/main.js
+++ src/Runtime/main.js
@@ -2,7 +2,15 @@
  * Runtime Class
  */
 class Runtime {
-	
+	/**
+	 * Runtime Class Constructor
+	 * @param {Renderer} renderer Prefered Renderer
+	 * @param {number} fps Frames Per Second
+	 */
+	constructor(renderer, fps = 60) {
+		this.fps = fps;
+		this.renderer = renderer;
+	}
 }
 
 module.exports = Runtime;
